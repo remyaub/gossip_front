3ee5cd4453000f69680322b7c09b140d686699f3
RemyAu <centratobey@gmail.com>
2019-02-05 15:57:36 +0100

    ok

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,5 +1,5 @@
 class Gossip < ApplicationRecord
   belongs_to :author, class_name: 'User'
   validates :title, presence: true
-  validates :content, presence: true, length: {minimum:2, maximum:30}
+  validates :content, presence: true, length: {minimum:5}
 end
